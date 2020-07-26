378151dfec600e01586926cb8705374acb588cb0
Paloma <palomacorreia>
2020-07-26 17:25:47 -0300

    Setup API

---
+++ src/main/java/com/produtos/APIRESTProdutos/ApirestProdutosApplication.java
@@ -0,0 +1,13 @@
+package com.produtos.APIRESTProdutos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApirestProdutosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApirestProdutosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/produtos/APIRESTProdutos/ApirestProdutosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.produtos.APIRESTProdutos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApirestProdutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

f6686cbacec78d09ec3eec5908458610a10d32db
Paloma <palomacorreiadasilva@gmail.com>
2020-08-01 21:49:22 -0300

    DATABASE + GET + POST

--- src/main/java/com/produtos/APIRESTProdutos/repository/ProdutoRepository.java
+++ src/main/java/com/produtos/APIRESTProdutos/repository/ProdutoRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.produtos.APIRESTProdutos.models.Produto;
 
 public interface ProdutoRepository extends JpaRepository<Produto, Long>{
-	//Produto findById(long id);
+	Produto findById(long id);
 
 }
